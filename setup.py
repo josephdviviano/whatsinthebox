b9371992311e551cb87b126a2904d72824315e86
Joseph Viviano <joseph@viviano.ca>
2021-01-06 09:55:29 -0500

    skeleton

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from setuptools import setup, find_packages
+
+setup(
+    name='witb',
+    version='0.0.1',
+    packages=find_packages(include=['witb', 'witb.*']),
+    python_requires='>=3.6',
+    install_requires=[
+        'flake8',
+        'flake8-docstrings',
+        'pytest>=4.6',
+        'sphinx',
+        'joblib',
+        'torch'],
+    entry_points={
+        'console_scripts': [
+            'main=witb.main:main'
+        ],
+    }
+)
